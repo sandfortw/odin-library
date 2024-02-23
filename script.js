49f681d6258969d00b5eb2f9598608a333ac19c0
Weston Sandfort <sandfortw@gmail.com>
2024-02-23 11:12:12 -0700

    Refactor for maintainability

--- script.js
+++ script.js
@@ -1,75 +1,100 @@
 const myLibrary = [];
+const bookButton = document.querySelector("#book-button");
+const dialogForm = document.querySelector("dialog");
+const submitButton = document.querySelector("#submit-button");
+const closeButton = document.querySelector("#close-button");
 
-function Book(title, author, pageCount, read=false){
+function Book(title, author, pageCount, read = false) {
   this.title = title;
   this.author = author;
   this.pageCount = pageCount;
   this.read = read;
-  this.info = () => {
-    return `${title} by ${author}, ${pageCount} pages, ${read ? 'read' : 'not read yet'}`;
-  }
 }
-
 
 function addBookToLibrary(book) {
-  myLibrary.push(book)
+  myLibrary.push(book);
 }
 
-function displayBooks(library){
-  tableBody = document.querySelector('tbody')
-  tableBody.innerHTML = '';
+const createBookRow = (object) => {
+  object.tr = document.createElement("tr");
+  const tr = generateRowContent(object);
+  tableBody.appendChild(tr);
+};
+
+//Helper methods for createBookRow:
+
+const generateRowContent = (object) => {
+  object.tr = addBookDetails(object);
+  object.tr = addDeleteButton(object);
+  object.tr = addReadButton(object);
+  return object.tr;
+};
+
+const addBookDetails = (object) => {
+  const attributes = ["title", "author", "pageCount", "read"];
+  attributes.forEach((attribute) => {
+    let td = document.createElement("td");
+    td.textContent = object.book[attribute];
+    object.tr.appendChild(td);
+  });
+  return object.tr;
+};
+
+const addDeleteButton = (object) => {
+  let deleteButton = document.createElement("button");
+  deleteButton.value = object.index;
+  deleteButton.textContent = "Delete";
+  deleteButton.addEventListener("click", () => {
+    delete myLibrary[deleteButton.value];
+    displayBooks(myLibrary);
+  });
+  object.tr.appendChild(deleteButton);
+  return object.tr;
+};
+
+const addReadButton = (object) => {
+  let readButton = document.createElement("button");
+  readButton.value = object.index;
+  readButton.textContent = "Toggle Read";
+  readButton.addEventListener("click", () => {
+    const libraryBook = myLibrary[readButton.value];
+    libraryBook.read = !libraryBook.read;
+    displayBooks(myLibrary);
+  });
+  object.tr.appendChild(readButton);
+  return object.tr;
+};
+//^^^End helper methods for createBookRow^^^
+
+function displayBooks(library) {
+  tableBody = document.querySelector("tbody");
+  tableBody.innerHTML = "";
   for (let index = 0; index < library.length; index++) {
     const book = library[index];
-    if(book != undefined) {
-      let tr = document.createElement("tr");
-      let attributes = ["title", "author", "pageCount", "read"];
-      attributes.forEach(attribute => {
-        let td = document.createElement("td")
-        td.textContent = book[attribute];
-        tr.appendChild(td)
-      })
-      let deleteButton = document.createElement("button");
-      deleteButton.value = index;
-      deleteButton.textContent = 'Delete'
-      deleteButton.addEventListener('click', () =>{
-        delete myLibrary[deleteButton.value]
-        displayBooks(myLibrary)
-      })
-
-      let readButton = document.createElement("button");
-      readButton.value = index;
-      readButton.textContent = 'Toggle Read'
-      readButton.addEventListener('click', () =>{
-        myLibrary[readButton.value].read = !myLibrary[readButton.value].read
-        displayBooks(myLibrary)
-      })
-      tr.appendChild(deleteButton)
-      tr.appendChild(readButton)
-      tableBody.appendChild(tr)
-    } 
+    if (book != undefined) {
+      createBookRow({ book: book, index: index });
+    }
   }
 }
 
-const bookButton = document.querySelector("#book-button")
-const dialogForm = document.querySelector('dialog')
-const submitButton = document.querySelector('#submit-button')
-
-bookButton.addEventListener('click', () =>{
+bookButton.addEventListener("click", () => {
   dialogForm.showModal();
-})
-
-const closeButton = document.querySelector("#close-button");
+});
 
 closeButton.addEventListener("click", () => {
   dialogForm.close();
 });
 
-submitButton.addEventListener("click", (event) =>{
+submitButton.addEventListener("click", (event) => {
   event.preventDefault();
-  const inputs = document.querySelectorAll("input")
-  const radioSelection = document.querySelector('input[name="read"]:checked')
-  const book = new Book(inputs[0].value, inputs[1].value, inputs[2].value, radioSelection.value)
-  addBookToLibrary(book)
-  displayBooks(myLibrary)
-})
-
+  const inputs = document.querySelectorAll("input");
+  const radioSelection = document.querySelector('input[name="read"]:checked');
+  const book = new Book(
+    inputs[0].value,
+    inputs[1].value,
+    inputs[2].value,
+    radioSelection.value
+  );
+  addBookToLibrary(book);
+  displayBooks(myLibrary);
+});
