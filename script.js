cb8b932e880f2aa55953a63e6eb6929b309b18a9
Weston Sandfort <sandfortw@gmail.com>
2024-02-22 18:34:25 -0700

    Create functions that can add books to table

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const myLibrary = [];
+
 function Book(title, author, pageCount, read=false){
   this.title = title;
   this.author = author;
@@ -7,3 +9,27 @@ function Book(title, author, pageCount, read=false){
     return `${title} by ${author}, ${pageCount} pages, ${read ? 'read' : 'not read yet'}`;
   }
 }
+
+
+function addBookToLibrary(book) {
+  myLibrary.push(book)
+}
+
+function displayBooks(library){
+  tableBody = document.querySelector('tbody')
+  library.forEach(book => {
+    let tr = document.createElement("tr");
+    let attributes = ["title", "author", "pageCount", "read"];
+    attributes.forEach(attribute => {
+      let td = document.createElement("td")
+      td.textContent = book[attribute];
+      tr.appendChild(td)
+    })
+    tableBody.appendChild(tr)
+  });
+}
+
+let book1 = new Book('Return of the King', 'Tolkien', '200')
+
+addBookToLibrary(book1)
+displayBooks(myLibrary)
